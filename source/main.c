f4aa5b64d8a28d7d85c7c38e12e659343ea08c33
Ivan Veloz <devel@ivanveloz.com>
2024-05-11 15:49:21 -0400

    Added demo for DNS lookup

--- source/main.c
+++ source/main.c
@@ -1,43 +1,67 @@
-/*---------------------------------------------------------------------------------
+/*
+ * Copyright (C) 2024 Ivan Veloz.  All Rights Reserved.
+ *
+ * SPDX-License-Identifier: MIT
+*/
 
----------------------------------------------------------------------------------*/
 #include <nds.h>
 #include <dswifi9.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <stdio.h>
+#include <assert.h>
 #include <core_sntp_client.h>
 
-//---------------------------------------------------------------------------------
+char * ntpurl = "us.pool.ntp.org";
+
 int main(void) {
-//---------------------------------------------------------------------------------
 
 	struct in_addr ip, gateway, mask, dns1, dns2;
 
-	consoleDemoInit();  //setup the sub screen for printing
+	consoleDemoInit();
 
-	iprintf("\n\n\tSimple Wifi Connection Demo\n\n");
-	iprintf("Connecting via WFC data ...\n");
+	iprintf("Connecting via WFC data.n");
 
 	if(!Wifi_InitDefault(WFC_CONNECT)) {
-		iprintf("Failed to connect!");
-	} else {
-
-		iprintf("Connected\n\n");
-
-		ip = Wifi_GetIPInfo(&gateway, &mask, &dns1, &dns2);
-		
-		iprintf("ip     : %s\n", inet_ntoa(ip) );
-		iprintf("gateway: %s\n", inet_ntoa(gateway) );
-		iprintf("mask   : %s\n", inet_ntoa(mask) );
-		iprintf("dns1   : %s\n", inet_ntoa(dns1) );
-		iprintf("dns2   : %s\n", inet_ntoa(dns2) );
-		
-		
+		iprintf("Connection failed.");
+		goto idle;
 	}
+
+	iprintf("Connected.\n\n");
+
+	ip = Wifi_GetIPInfo(&gateway, &mask, &dns1, &dns2);
 	
-	
-	
+	iprintf("ip     : %s\n", inet_ntoa(ip) );
+	iprintf("gateway: %s\n", inet_ntoa(gateway) );
+	iprintf("mask   : %s\n", inet_ntoa(mask) );
+	iprintf("dns1   : %s\n", inet_ntoa(dns1) );
+	iprintf("dns2   : %s\n", inet_ntoa(dns2) );
+
+	iprintf("ntp url: %s\n",ntpurl);
+
+	struct hostent * ntphost = gethostbyname(ntpurl);
+	if(ntphost == NULL) {
+		iprintf("Error: failed to get hostname");
+		goto idle;
+	}
+
+	iprintf("h_name : %s\n",ntphost->h_name);
+
+	for(size_t i=0; ntphost->h_aliases[i] != NULL; i++) {
+		iprintf("h_alias: %s\n",ntphost->h_aliases[i]);
+	}
+
+	/* Assert that we're dealing with IPv4 addresses, 32 bit lengths. */
+	assert(ntphost->h_addrtype == AF_INET);
+	assert(ntphost->h_length == 4);
+
+	for(int i=0; ntphost->h_addr_list[i] != NULL; i++) {
+		struct in_addr a = *(struct in_addr *)ntphost->h_addr_list[i];
+		iprintf("h_addr : %s\n", inet_ntoa(a));
+	}
+
+	idle:
 	while(1) {
 		swiWaitForVBlank();
 		int keys = keysDown();
