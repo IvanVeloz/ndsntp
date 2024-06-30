2c52bfa6d150c5b6647d5905a639e6f57542a451
Ivan Veloz <devel@ivanveloz.com>
2024-06-29 20:02:42 -0400

    Implemented menu with spinboxes and three views.

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -14,6 +14,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <assert.h>
 #include <time.h>
 #include <core_sntp_client.h>
@@ -27,16 +28,19 @@
 #define NTP_RECEIVE_WAIT_TIME_MS		1000
 
 #define IF_DIAGNOSTICS					\
-	swiWaitForVBlank();					\
-	scanKeys();							\
 	if(keysCurrent() & (KEY_L|KEY_R))
 
 const char * ntpurl = "us.pool.ntp.org";
+
+enum Menu { MENU_TZ, MENU_SYNCING, MENU_SYNCED, MENU_EXIT };
 
 void spinloop(void);
 unsigned int sleeprtc(unsigned int seconds);
 void printIpInfo(void);
 int printNsLookup(void);
+int syncTime(int retries);
+enum Menu displayTZMenu(void);
+enum Menu displaySyncedMenu(void);
 
 int main(void) {
 
@@ -48,71 +52,49 @@ int main(void) {
 		goto end;
 	}
 	
+	scanKeys();
 	IF_DIAGNOSTICS {
 		printIpInfo();
 		sleeprtc(2);
 	}
 
+	scanKeys();
 	IF_DIAGNOSTICS {
 		if(printNsLookup()) spinloop();
 		else sleeprtc(2);
 	}
 	
-	uint8_t netBuffer[SNTP_PACKET_BASE_SIZE];
-	NetworkContext_t netContext = {
-		.udpSocket = socket(AF_INET, SOCK_DGRAM, 0)
-	};
-	SntpServerInfo_t server = {
-		.port = SNTP_DEFAULT_SERVER_PORT,
-		.pServerName = ntpurl,
-		.serverNameLen = strlen(ntpurl)
-	};
-    UdpTransportInterface_t udpTransportIntf = {
-		.pUserContext = &netContext,
-		.sendTo = sntpUdpSend,
-		.recvFrom = sntpUdpRecv
-	};
-	SntpContext_t sntpContext;
-	
-    SntpStatus_t status = Sntp_Init( &sntpContext,
-                                     &server,
-                                     sizeof(server) / sizeof(SntpServerInfo_t),
-                                     NTP_TIMEOUT,
-                                     netBuffer,
-                                     SNTP_PACKET_BASE_SIZE,
-                                     sntpResolveDns,
-                                     sntpGetTime,
-                                     sntpSetTime,
-                                     &udpTransportIntf,
-                                     NULL );
-	
-	assert(status == SntpSuccess);
-
+	enum Menu menu = MENU_TZ;
 	while( 1 )
     {
-        status = Sntp_SendTimeRequest( &sntpContext,
-                                       rand() % UINT32_MAX,
-                                       NTP_SEND_WAIT_TIME_MS );
-		printf("Sntp_SendTimeRequest = %u\n", (unsigned int)status);
-		if ( status != SntpSuccess ) continue;
- 
-        do
-        {
-            status = Sntp_ReceiveTimeResponse( &sntpContext, NTP_RECEIVE_WAIT_TIME_MS );
-        } while( status == SntpNoResponseReceived );
- 		printf("Sntp_ReceiveTimeResponse = %u\n", (unsigned int)status);
-        if ( status != SntpSuccess ) continue;
-
-		printf("\n\n\nRTC is %llu\n", time(NULL));
-		printf("Press A to sync again.\n"
-			   "Press Start to power off.\n");
-		while(1) {
-			swiWaitForVBlank();
-			scanKeys();
-			int keys = keysDown();
-			if(keys & KEY_START) goto end;
-			if(keys & KEY_A) break;
+		switch(menu) {
+			case MENU_TZ:
+				menu = displayTZMenu();
+				break;
+			case MENU_SYNCING:
+				swiWaitForVBlank();
+				scanKeys();
+				printf("\x1b[2J"); // Clear console
+				printf("\n\n");
+				if(syncTime(5)) {
+					printf("Couldn't connect to time server(s)!\n");
+				}
+				IF_DIAGNOSTICS {
+					sleeprtc(2);
+				}
+				menu = MENU_SYNCED;
+				break;
+			case MENU_SYNCED:
+				menu = displaySyncedMenu();
+				break;
+			case MENU_EXIT:
+				goto end;
+			default:
+				menu = MENU_TZ;
 		}
+
+
+
 		printf("\n\n");
     }
 	end:
@@ -142,6 +124,8 @@ unsigned int sleeprtc(unsigned int seconds) {
 	return 0;
 }
 
+/* Print IP address information for diagnostics.
+ */
 void printIpInfo(void) {
 	struct in_addr ip, gateway, mask, dns1, dns2;
 	ip = Wifi_GetIPInfo(&gateway, &mask, &dns1, &dns2);
@@ -153,6 +137,8 @@ void printIpInfo(void) {
 	printf("ntp url: %s\n",ntpurl);
 }
 
+/* Lookup the NTP hostname and print the entry(ies) given by the DNS server.
+ */
 int printNsLookup(void) {
 	struct hostent * ntphost = gethostbyname(ntpurl);
 	if(ntphost == NULL) {
@@ -174,4 +160,156 @@ int printNsLookup(void) {
 	return 0;
 }
 
+/* Connect to an NTP server and set the time (assumes locale is set; if not set
+ * it defaults to UTC).
+ */
+int syncTime(int retries)
+{
+	uint8_t netBuffer[SNTP_PACKET_BASE_SIZE];
+	NetworkContext_t netContext = {
+		.udpSocket = socket(AF_INET, SOCK_DGRAM, 0)
+	};
+	SntpServerInfo_t server = {
+		.port = SNTP_DEFAULT_SERVER_PORT,
+		.pServerName = ntpurl,
+		.serverNameLen = strlen(ntpurl)
+	};
+    UdpTransportInterface_t udpTransportIntf = {
+		.pUserContext = &netContext,
+		.sendTo = sntpUdpSend,
+		.recvFrom = sntpUdpRecv
+	};
+	SntpContext_t sntpContext;
+	
+    SntpStatus_t status = Sntp_Init( &sntpContext,
+                                     &server,
+                                     sizeof(server) / sizeof(SntpServerInfo_t),
+                                     NTP_TIMEOUT,
+                                     netBuffer,
+                                     SNTP_PACKET_BASE_SIZE,
+                                     sntpResolveDns,
+                                     sntpGetTime,
+                                     sntpSetTime,
+                                     &udpTransportIntf,
+                                     NULL );
+	if(status != SntpSuccess) {
+		LogError(("Failed to initialize SNTP.\n"));
+		return -1;
+	}
 
+	int i,j;
+	for(i=0, j=0; i<retries; i++) {
+		status = Sntp_SendTimeRequest( &sntpContext,
+										rand() % UINT32_MAX,
+										NTP_SEND_WAIT_TIME_MS );
+		if ( status != SntpSuccess ) continue;
+		do {
+            status = Sntp_ReceiveTimeResponse( &sntpContext, NTP_RECEIVE_WAIT_TIME_MS );
+        } while( status == SntpNoResponseReceived );
+		if ( status != SntpSuccess ) continue;
+		break;
+	}
+
+	if(i>=retries || j>=retries) {
+		LogError(("Failed to request SNTP time.\n"));
+		return -1;
+	}
+	else 
+		return 0;
+}
+
+/* Display the timezone setting dialog. 
+ * @returns an `enum Menu` with the next menu that should be displayed.
+ */
+enum Menu displayTZMenu(void)
+{
+	enum Selection {s_hour, s_minute};
+	struct Tz {int8_t hour; uint8_t minute;};
+
+	static enum Selection sel = s_hour;
+	static struct Tz tz = {.hour=0, .minute=0};
+
+	swiWaitForVBlank();
+	printf("\x1b[2J"); // Clear console
+	const int coord_x[2] = {
+		5, 8
+	};
+	printf("\n\nTimezone:\n\n");
+	printf("\x1b[%dC^\n", coord_x[sel]);
+	printf("UTC%+03i:%02u\n", tz.hour, tz.minute%60);
+    printf("\x1b[%dCv\n", coord_x[sel]);
+
+	printf(	"\n\n\n\n\n\n\n\n\n\n\n\n"
+			"Press A to sync time.\n"
+			"Press Start to exit.");
+
+	scanKeys();
+	uint16_t keys = keysDownRepeat();
+
+	if(keys & KEY_A) {
+		tz.minute = tz.minute % 60;
+		// TODO: change the system locale right here
+		return MENU_SYNCING;
+	}
+	else if(keys & KEY_LEFT) {
+		if(sel>s_hour) sel--;
+	}
+	else if(keys & KEY_RIGHT) {
+		if(sel<s_minute) sel++;
+	}
+	else if(keys & KEY_UP) {
+		switch(sel) {
+			case s_hour:
+				if(tz.hour >= 16) tz.hour = 16;
+				else tz.hour += 1;
+				break;
+			case s_minute:
+				if(tz.minute >= 120) tz.minute = 120;
+				else if(tz.minute >= 60) tz.minute += 1;
+				else tz.minute += 15;
+				break;
+		}
+	}
+	else if(keys & KEY_DOWN) {
+		switch(sel) {
+			case s_hour:
+				if(tz.hour <= -16) tz.hour = 16;
+				else tz.hour -= 1;
+				break;
+			case s_minute:
+				if(tz.minute <= 0) tz.minute = 0;
+				else if(tz.minute > 60) tz.minute -= 1;
+				else tz.minute -= 15;
+				break;
+		}
+	}
+	else if(keys & KEY_START) {
+		return MENU_EXIT;
+	}
+
+	return MENU_TZ;
+
+}
+
+enum Menu displaySyncedMenu(void)
+{
+	char str[100];
+	time_t t = time(NULL);
+	struct tm *tmp = localtime(&t);
+	swiWaitForVBlank();
+	printf("\x1b[2J"); // Clear console
+	printf("\n\nCurrent time:\n\n\n");
+	if (strftime(str, sizeof(str), "%Y-%m-%dT%H:%M:%S%z", tmp) == 0)
+		snprintf(str, sizeof(str), "Failed to get time");
+	printf("%s\n", str);
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n");
+	printf("Press A to sync again.\n"
+		"Press B to go back.\n"
+		"Press Start to exit.\n");
+	scanKeys();
+	int keys = keysDown();
+	if(keys & KEY_START) return MENU_EXIT;
+	if(keys & KEY_A) return MENU_SYNCING;
+	if(keys & KEY_B) return MENU_TZ;
+	return MENU_SYNCED;
+}
